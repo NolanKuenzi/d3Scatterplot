2b62c87e11514c7d941c523dceda4bd32f4d870c
Nolan Kuenzi <mr.nolank@gmail.com>
2018-10-22 23:12:06 -0700

    Layout update

--- src/js/app.js
+++ src/js/app.js
@@ -17,44 +17,44 @@ svg = svg.append("g")
          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 function scatterPlotGraph() {
-const req = new XMLHttpRequest();
+  const req = new XMLHttpRequest();
   req.open("GET","https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json", true);
   req.send();
   req.onload = function() {
-const data = JSON.parse(req.responseText);
+  const data = JSON.parse(req.responseText);
 
-const parseYear = d3.timeParse("%Y");
+  const parseYear = d3.timeParse("%Y");
 
-const xAxisScale = d3.scaleTime()
-                     .domain([d3.min(data, d => parseYear(d.Year - 1)), d3.max(data, d => parseYear(d.Year + 1))])
-                     .range([0, viewWidth]);
+  const xAxisScale = d3.scaleTime()
+                       .domain([d3.min(data, d => parseYear(d.Year - 1)), d3.max(data, d => parseYear(d.Year + 1))])
+                       .range([0, viewWidth]);
 
-const xAxisCall = d3.axisBottom(xAxisScale);
+  const xAxisCall = d3.axisBottom(xAxisScale);
 
-const xAxis = svg.append("g")
-                 .attr("id", "x-axis")
-                 .attr("transform", "translate(" + 0 + "," + viewHeight + ")")
-                 .call(xAxisCall)
-                 .selectAll(".tick line");
+  const xAxis = svg.append("g")
+                   .attr("id", "x-axis")
+                   .attr("transform", "translate(" + 0 + "," + viewHeight + ")")
+                   .call(xAxisCall)
+                   .selectAll(".tick line");
 
-const parseTime = d3.timeParse("%M:%S");
+  const parseTime = d3.timeParse("%M:%S");
 
-const yAxisScale = d3.scaleTime()
-                     .domain([d3.max(data, d => parseTime(d.Time)), d3.min(data, d => parseTime(d.Time))])
-                     .range([viewHeight, 0]);
+  const yAxisScale = d3.scaleTime()
+                       .domain([d3.max(data, d => parseTime(d.Time)), d3.min(data, d => parseTime(d.Time))])
+                       .range([viewHeight, 0]);
 
-const mins_secs = d3.timeFormat("%M:%S");
+  const mins_secs = d3.timeFormat("%M:%S");
 
-const yAxisCall = d3.axisLeft(yAxisScale)
-                    .tickFormat(mins_secs);
+  const yAxisCall = d3.axisLeft(yAxisScale)
+                      .tickFormat(mins_secs);
 
-const yAxis = svg.append("g")
-                 .attr("id", "y-axis")
-                 .call(yAxisCall);	
+  const yAxis = svg.append("g")
+                   .attr("id", "y-axis")
+                   .call(yAxisCall);	
 
-const toolTip = d3.select("body")
-                  .append("div")
-                  .attr("id", "toolTip");		
+  const toolTip = d3.select("body")
+                    .append("div")
+                    .attr("id", "toolTip");		
 							  
   svg.append("rect")
      .attr("id", "legend")
@@ -92,40 +92,40 @@ const toolTip = d3.select("body")
      .text("Time in Minutes")
      .style("font-size", "20px");
 
-const circles = d3.select("svg")
-                  .append("g")
-                  .selectAll("circle")
-                  .data(data)
-                  .enter()
-                  .append("circle")
-                  .attr("class", "dot")
-                  .attr("data-xvalue", d => d.Year)
-                  .attr("data-yvalue", d => d.Time)
-                  .attr("cx", d => xAxisScale(parseYear(d.Year)) + margin.left)
-                  .attr("cy", d => yAxisScale(parseTime(d.Time)) + margin.top)
-                  .attr("r", 7)
-                  .style("fill", d => d.Doping === "" ? "#00bfff" : "#830303")
-                  .style("stroke", "black")
-                  .on("mouseover", function(d) {
-                    toolTip
-                      .attr("data-year", d.Year)
-                      .style("left", d3.event.pageX + 15 + "px")
-                      .style("top", d3.event.pageY + -15 + "px")
-                      .style("display", "inline-block")
-                      .html(() => `${d.Name}: ${d.Nationality} <br> Year: ${d.Year}, Time: ${d.Time} <br><br> ${d.Doping}`);
-                    }) 
-                  .on("mouseleave", function(d) {
-                    toolTip
-                      .style("display", "none");
-                  }); 
-    /* For Mobile Devices */
-    const clear = document.querySelector("body");
-    clear.addEventListener("touchstart", function(e) {
-      if (e.target.className.baseVal !== "dot") {
-        toolTip
-          .style("display", "none");
-      }
-    });
+  const circles = d3.select("svg")
+                    .append("g")
+                    .selectAll("circle")
+                    .data(data)
+                    .enter()
+                    .append("circle")
+                    .attr("class", "dot")
+                    .attr("data-xvalue", d => d.Year)
+                    .attr("data-yvalue", d => d.Time)
+                    .attr("cx", d => xAxisScale(parseYear(d.Year)) + margin.left)
+                    .attr("cy", d => yAxisScale(parseTime(d.Time)) + margin.top)
+                    .attr("r", 7)
+                    .style("fill", d => d.Doping === "" ? "#00bfff" : "#830303")
+                    .style("stroke", "black")
+                    .on("mouseover", function(d) {
+                      toolTip
+                        .attr("data-year", d.Year)
+                        .style("left", d3.event.pageX + 15 + "px")
+                        .style("top", d3.event.pageY + -15 + "px")
+                        .style("display", "inline-block")
+                        .html(() => `${d.Name}: ${d.Nationality} <br> Year: ${d.Year}, Time: ${d.Time} <br><br> ${d.Doping}`);
+                      }) 
+                    .on("mouseleave", function(d) {
+                      toolTip
+                        .style("display", "none");
+                    }); 
+      /* For Mobile Devices */
+      const clear = document.querySelector("body");
+      clear.addEventListener("touchstart", function(e) {
+        if (e.target.className.baseVal !== "dot") {
+          toolTip
+            .style("display", "none");
+        }
+      });
   
   };
 }
